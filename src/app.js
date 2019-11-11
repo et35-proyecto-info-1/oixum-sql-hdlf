c4ec3b6bf8f53bb09044421cbb2d5d98dcb900b1
Mi Nombre <dou@gmail.com>
2019-11-11 17:09:14 -0300

    Primer commit

--- src/app.js
+++ src/app.js
@@ -3,24 +3,31 @@ var connection = require('./connection');
 
 var app = express();
 
+app.use(express.urlencoded({ extended: false }));
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
 app.get('/', function(req, res) {
-  res.send('¡Hola ET 35!');
+  res.render('home', { 
+    mensaje: '¡Hola ET 35!', 
+    texto: 'Esto también sale del servidor',
+    fecha: new Date()
+  });
+  res.render('home');
 });
 
 app.get('/query', function(req, res) {
-  connection.query('SELECT * FROM estudiantes', function (error, results, fields) {
+  connection.query('SELECT ' + req.query.campos +  ' FROM estudiantes', function (error, results, fields) {
     if (error) throw error;
     res.json(results);
+app.post('/', function(req, res) {
+  const query = req.body.query;
+  connection.query(query, function (error, results, fields) {
+    res.render('home', {
+      resultados: JSON.stringify(results),
+      columnas: JSON.stringify(fields),
+      error: error
+    });
   });
-});
-
-app.listen(3000, function() {
-  connection.connect();
-  console.log('Entrá a http://localhost:3000 desde tu navegador para ver qué devuelve esto');
-});
-
-process.on('SIGINT', function() {
-  console.log('Cerrando la conexión con la base de datos')
-  connection.end();
-  process.exit(1);
 });
