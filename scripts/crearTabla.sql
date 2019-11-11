ef29cabcc5331fe873d24aaab9af252f737dab5c
Mi Nombre <dou@gmail.com>
2019-11-11 17:41:15 -0300

    Commit Insert

--- scripts/crearTabla.sql
+++ scripts/crearTabla.sql
@@ -7,3 +7,10 @@ CREATE TABLE IF NOT EXISTS test.estudiantes (
 	fechaNacimiento DATE NOT NULL,
 	CONSTRAINT estudiantes_PK PRIMARY KEY (id)
 );
+
+INSERT INTO test.estudiantes (nombre, comidaPreferida, fechaNacimiento)
+VALUES 
+  ('Alan Castro', 'Fideos con tuco', '1993-08-21'),
+  ('Camila Sierra', 'Milanesa con pure', '1957-02-29'),
+  ('Matias Rodriguez', 'Estofado de pollo', '1961-04-05'),
+  ('Melanie Torres', 'Ensalada cesar', '1934-02-14');
