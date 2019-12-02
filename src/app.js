d79cf2759c62b86f971d4380716e5b5001a8b5c8
Matias Humano <Alan.humano@gmail.com>
2019-12-02 05:37:28 -0300

    Update Pug

--- src/app.js
+++ src/app.js
@@ -1,25 +1,23 @@
 var express = require('express');
 var connection = require('./connection');
-var exphbs  = require('express-handlebars');
 var path = require('path');
 var app = express();
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+app.set('view engine', 'pug');
 
 app.get('/', function(req, res) {
-  res.render('home');
+  res.render('index');
 });
 
 app.post('/', function(req, res) {
   const query = req.body.query;
   connection.query(query, function (error, results, fields) {
-    res.render('home', {
-      resultados: results,
-      columnas: JSON.stringify(fields),
+    res.render('index', {
+      results: results,
+      fields:fields,
       error: error
     });
   });
