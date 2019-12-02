f749ff4edf1d1668d99b7f66dfa40454952feb75
Matias Humano <Alan.humano@gmail.com>
2019-12-02 05:24:49 -0300

    Final

--- scripts/crearTabla.sql
+++ scripts/crearTabla.sql
@@ -14,3 +14,18 @@ VALUES
   ('Camila Sierra', 'Milanesa con pure', '1957-02-29'),
   ('Matias Rodriguez', 'Estofado de pollo', '1961-04-05'),
   ('Melanie Torres', 'Ensalada cesar', '1934-02-14');
+  
+  CREATE TABLE IF NOT EXISTS test.profesores (
+	id INT NOT NULL AUTO_INCREMENT,
+	nombre varchar(100) NOT NULL,
+	Materia varchar(100) NULL,
+	fechaNacimiento DATE NOT NULL,
+	CONSTRAINT estudiantes_PK PRIMARY KEY (id)
+);
+
+INSERT INTO test.profesores (nombre, materia, fechaNacimiento)
+VALUES
+  ('Patricia Maguire', 'Lengua', '1965-08-27'),
+  ('Manuel Salgado', 'Quimica', '1970-05-13'),
+  ('Jorge Muscio', 'Fisica', '1968-02-08'),
+  ('Sandra Daujan', 'Base de datos', '1961-04-20');
